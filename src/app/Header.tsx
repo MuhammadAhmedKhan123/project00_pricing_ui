b08a5056c5afa65e7e2754a6dcaf0ed5a4b2627d
MuhammadAhmedKhan123 <115992193+MuhammadAhmedKhan123@users.noreply.github.com>
2023-02-18 04:23:04 -0800

    Update Header.tsx

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -6,7 +6,7 @@ import React from 'react';
 function Header() {
   return (
     <ChakraProvider> 
-    <Box textAlign={{md:"center"}} pl={{base:'20px'}} bg="#6B46C1" pt={{base:"50px", md:"90px"}}  pb='220px' textColor='white'>
+    <Box textAlign={{md:"center"}} pl={{sm:'20px'}} bg="#6B46C1" pt={{sm:"50px", md:"90px"}}  pb='220px' textColor='white'>
         <Heading>Simple pricing for your business</Heading>
         <Text pt='16px'>Plans that are carefully crafted ti suit your business</Text>
     </Box>
